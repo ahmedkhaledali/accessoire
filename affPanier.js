8229e1a83ba99ff64a778ca62a7828f0840bc637
aliahmedkhaled <ali123.ahmed.khaled1993@hotmail.com>
2021-04-19 02:27:13 +0200

    desing

---
+++ affPanier.js
@@ -0,0 +1,86 @@
+
+localStorage.getItem("test")
+console.log(localStorage.getItem("test"))
+    let panierTab=JSON.parse(localStorage.getItem("test"))
+
+console.log(panierTab)
+var somme=0;
+
+for (var i = 0; i < panierTab.length; i++){
+    var tab = panierTab[i];
+        let prix=Number(tab.prix)
+        somme=somme+prix;
+
+
+
+    var node1 = document.createElement("p");
+    var node2 = document.createElement("p");
+    var node3 = document.createElement("p");
+    var node4 = document.createElement("img");
+node4.src="http://127.0.0.1:5500/"
+    node4.style.height="50px"
+    var node5= document.createElement('input');
+    var node6 = document.createElement("input");
+    var node7 = document.createElement("p");
+
+
+
+    var textnode = document.createTextNode(tab.nom);
+    var textnode2 = document.createTextNode(tab.prix);
+    var textnode1 = document.createTextNode(tab.cat);
+    var textnode3 = document.createTextNode(tab.img);
+    var textnode4 = document.createTextNode("Total"+" "+ somme);
+
+
+        
+    node5.setAttribute('type','button');
+    node5.setAttribute('name','supprimer');
+    node5.setAttribute('value','supprimer');
+    document.getElementById("supp").appendChild(node5);
+    node5.addEventListener('onclick',supp());
+
+
+     
+    node6.setAttribute("type", "number");
+    node6.setAttribute("value", "1");
+    document.getElementById("qte").appendChild(node6);
+    node6.style.width="50px"
+
+    node1.appendChild(textnode);
+    document.getElementById("nom").appendChild(node1);
+    
+    node2.appendChild(textnode1);
+    document.getElementById("cat").appendChild(node2);
+
+    node3.appendChild(textnode2);
+    document.getElementById("prix").appendChild(node3);
+
+    node4.appendChild(textnode3);
+    document.getElementById("img").appendChild(node4);
+
+
+  }
+  node7.appendChild(textnode4)
+  document.querySelector("h1").appendChild(node7);
+  
+function supp(){
+    
+    console.log()
+}
+
+
+
+
+
+
+
+
+
+
+  //document.getElementById('dd').innerHTML=`<h1>${tab.nom}</h1>`
+  
+  //for (var key in tab){
+    //var value = tab[key];
+    
+      //console.log(value+" "+key);} pour verifier votre tableaux
+  
--- panier.js
+++ panier.js
@@ -8,44 +8,103 @@ function ajoutPanier(x){
    var myObj = {
     "nom" : document.getElementById(x.id).getAttribute('datalib'),    //your artist variable
     "prix" : document.getElementById(x.id).getAttribute('dataprix'),  //your title variable
-    "qte":document.getElementById(x.id).getAttribute('dataqte')
+    "cat":document.getElementById(x.id).getAttribute('datacategorie'),
+    "img":document.getElementById(x.id).getAttribute('dataimg')
 };
+panier.push( myObj )
+console.log(panier)
 
 let a=myObj.nom
 let b=myObj.prix
-let c=myObj.qte 
+let c=myObj.cat 
+let d=myObj.img
 
-let obj=JSON.stringify(myObj)
+let obj=JSON.stringify(panier)
+console.log(obj)
 
-var node1 = document.createElement("p");
+
+    var node1 = document.createElement("p");
     var node2 = document.createElement("p");
     var node3 = document.createElement("p");
+    var node4 = document.createElement("img");
+    node4.src=d
+    node4.style.height="50px"
+  
     
+    var textnode1 = document.createTextNode(a);
+    var textnode2 = document.createTextNode(b);
+    var textnode3 = document.createTextNode(c);
+    var textnode4 = document.createTextNode(d);
     
-    var textnode = document.createTextNode(a);
-    var textnode1 = document.createTextNode(b);
-    var textnode2 = document.createTextNode(c);
+    node1.appendChild(textnode1);
+    document.getElementById("nom").appendChild(node1);
     
-    node1.appendChild(textnode);
-    document.getElementById("tab").appendChild(node1);
-    
-    node2.appendChild(textnode1);
-    document.getElementById("tab").appendChild(node2);
+    node2.appendChild(textnode2);
+    document.getElementById("prix").appendChild(node2);
 
-    node3.appendChild(textnode2);
-    document.getElementById("tab").appendChild(node3);
+    node3.appendChild(textnode3);
+    document.getElementById("Categories").appendChild(node3);
 
-    
+    node4.appendChild(textnode4);
+    document.getElementById("image").appendChild(node4);
   
 
     localStorage.setItem("test",obj)
     localStorage.getItem("test")
-
-    
-
     const local=JSON.parse(localStorage.getItem("test"))
   
-panier.push( myObj )
-console.log(panier)
 }
+//////////////////////////////////////////////////////////
+let compt = document.querySelectorAll('.touche')
+for(let i=0;i<compt.length;i++){
+    compt[i].addEventListener("click", () => {
+        kl()
+    })
+}
+function kl(){
+    let local=localStorage.getItem("kl")
+    local=parseInt(local);
+    if (local){
+      localStorage.setItem("kl",local+1)
+      document.getElementById('val').innerHTML=local+1
+    }
+    else{
+        localStorage.setItem("kl",1)
+        document.getElementById('val').innerHTML=1
+    }
+}
+
+
+
+function recherche(){
+
+  document.getElementById("Element_yahsrhom_lkol").innerHTML = ""
+
+  var form = document.getElementById("filtreForm");
+  let filtre= form.elements["flexFiltre"].value;
+  var data_filter = mesArticles.filter( element => element.cat ==filtre);
+
+ 
+  document.getElementById("Element_yahsrhom_lkol").innerHTML = 
+  `<div class="col-md-3"><div class="card shadow p-3 mb-5 bg-white rounded"><img src="img22.jpg" class="card-img-top" alt="..." class="w-20 h-20">
+       <div class="card-body">`+value[key]+
+         `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       </div>
+       <button class="touche" id="art-4" datalib="Africain-Bracletes" dataprix="100" datacategorie ="Bracelets" dataimg="http://127.0.0.1:5500/img22.jpg"onclick="ajoutPanier(this)">Click me</button></div></div>`;
+  
+     ///fin pour
+ 
+   //afficher filtre selectionné
+  //console.log(filtre);
+ 
+ 
+  //ajouter quantité 
+   //input number 3 4 wala tzyd  w tna9es
+   //event js aal input wa9t tetbadel valeur 
+   //tetbadel f objet mteek
+ }
+
+  
+  
+
 
