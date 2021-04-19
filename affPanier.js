fbb13f8a38d47c4dbdaf71b0ca2ff96096701b6d
aliahmedkhaled <ali123.ahmed.khaled1993@hotmail.com>
2021-04-19 23:35:13 +0200

    final

--- affPanier.js
+++ affPanier.js
@@ -1,3 +1,8 @@
+function mood() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 
 localStorage.getItem("test")
 console.log(localStorage.getItem("test"))
@@ -5,89 +10,40 @@ console.log(localStorage.getItem("test"))
 
 console.log(panierTab)
 var somme=0;
-
+let total = document.querySelector('.total')
+let cart = document.querySelector('.item1-card')
 for (var i = 0; i < panierTab.length; i++){
     var tab = panierTab[i];
         let prix=Number(tab.prix)
         somme=somme+prix;
 
-
-
-    var node1 = document.createElement("p");
-    var node2 = document.createElement("p");
-    var node3 = document.createElement("p");
-    var node4 = document.createElement("img");
-node4.src="http://127.0.0.1:5501/panier.html"
-    node4.style.height="50px"
-    var node5= document.createElement('input');
-    var node6 = document.createElement("input");
-    var node7 = document.createElement("p");
-
-
-
-    var textnode = document.createTextNode(tab.nom);
-    var textnode2 = document.createTextNode(tab.prix);
-    var textnode1 = document.createTextNode(tab.cat);
-    var textnode3 = document.createTextNode(tab.img);
-    var textnode4 = document.createTextNode("Total"+" "+ somme);
-
-
-        
-    node5.setAttribute('type','button');
-    node5.setAttribute('name','supprimer');
-    node5.setAttribute('value','supprimer');
-    document.getElementById("supp").appendChild(node5);
-    node5.addEventListener('onclick',supp());
-
-
-     
-    node6.setAttribute("type", "number");
-    node6.setAttribute("value", "1");
-    document.getElementById("qte").appendChild(node6);
-    node6.style.width="50px"
-
-    node1.appendChild(textnode);
-    document.getElementById("nom").appendChild(node1);
-    
-    node2.appendChild(textnode1);
-    document.getElementById("cat").appendChild(node2);
-
-    node3.appendChild(textnode2);
-    document.getElementById("prix").appendChild(node3);
-
-    node4.appendChild(textnode3);
-    document.getElementById("img").appendChild(node4);
-
-
-  }
-  node7.appendChild(textnode4)
-  document.querySelector("h1").appendChild(node7);
-  
-function supp(){
-    
-  let clearCart = document.querySelector(".supp")
-  clearCart.addEventListener("click", () => {
-  localStorage.removeItem("productsInCart")
-  localStorage.removeItem("cardNumbers")
-  localStorage.removeItem("totalPrice")
-  productSection.innerHTML = ''
-  document.querySelector('#badge-cart').innerHTML = 0
-  })
+console.log(tab.nom)
+cart.innerHTML+=` <div class="row mt-3 ">
+<div class="col-md-3">
+  <img src="${tab.img}" class="img"style="height:50px;width:50px" >
+</div>
+<div class="col-md-2">
+  <strong>${tab.nom}</strong>
+</div>
+<div class="col-md-3">
+  <strong>${tab.cat}</strong>
+</div>
+<div class="col-md-2 d-flex" >
+<strong>${tab.prix}</strong>
+</div>
+</div>`
 }
 
+console.log(somme)
+total.innerHTML=`<br><h1>Total tous les articles est ${somme}</h1>`
 
 
-
-
-
-
-
-
-
-  //document.getElementById('dd').innerHTML=`<h1>${tab.nom}</h1>`
-  
-  //for (var key in tab){
-    //var value = tab[key];
+  function supp(){
     
-      //console.log(value+" "+key);} pour verifier votre tableaux
-  
+    let total = document.querySelector('.total')
+let cart = document.querySelector('.item1-card')
+     localStorage.clear()
+    cart.innerHTML=""
+    total.innerHTML=""
+  }
+
--- panier.js
+++ panier.js
@@ -11,9 +11,12 @@ function ajoutPanier(x){
     "cat":document.getElementById(x.id).getAttribute('datacategorie'),
     "img":document.getElementById(x.id).getAttribute('dataimg')
 };
+
 panier.push( myObj )
 console.log(panier)
 let item = document.querySelector('.item-card')
+
+
 let a=myObj.nom
 let b=myObj.prix
 let c=myObj.cat 
@@ -22,9 +25,9 @@ let d=myObj.img
 let obj=JSON.stringify(panier)
 console.log(obj)
 
- 
 
 
+  
 item.innerHTML+=`
      <div class="row mt-3 ">
      <div class="col-md-3">
@@ -40,6 +43,7 @@ item.innerHTML+=`
      <strong>${b}</strong>
      </div>
    </div>`
+
 
     localStorage.setItem("test",obj)
     localStorage.getItem("test")
@@ -65,49 +69,5 @@ function kl(){
         document.getElementById('val').innerHTML=1
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function recherche(){
-
-  document.getElementById("Element_yahsrhom_lkol").innerHTML = ""
-
-  var form = document.getElementById("filtreForm");
-  let filtre= form.elements["flexFiltre"].value;
-  var data_filter = mesArticles.filter( element => element.cat ==filtre);
-
- 
-  document.getElementById("Element_yahsrhom_lkol").innerHTML = 
-  `<div class="col-md-3"><div class="card shadow p-3 mb-5 bg-white rounded"><img src="img22.jpg" class="card-img-top" alt="..." class="w-20 h-20">
-       <div class="card-body">`+value[key]+
-         `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-       </div>
-       <button class="touche" id="art-4" datalib="Africain-Bracletes" dataprix="100" datacategorie ="Bracelets" dataimg="http://127.0.0.1:5500/img22.jpg"onclick="ajoutPanier(this)">Click me</button></div></div>`;
-  
-     ///fin pour
- 
-   //afficher filtre selectionné
-  //console.log(filtre);
- 
- 
-  //ajouter quantité 
-   //input number 3 4 wala tzyd  w tna9es
-   //event js aal input wa9t tetbadel valeur 
-   //tetbadel f objet mteek
- }
-
-  
-  
 
 
